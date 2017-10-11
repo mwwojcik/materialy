1f5aab8d705076e9ae3b21b5df13dc275d2464c3
Wójcik Mariusz <Mariusz.Wojcik@poland.asseco.corp>
2017-10-11 14:34:21 +0100

    init

---
+++ angular2/angular2.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1507728680015" ID="ID_1152306162" MODIFIED="1507728806325" TEXT="Angular 2 - Notatki">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1507728706393" ID="ID_761200037" MODIFIED="1507728739727" POSITION="right" TEXT="Dyrektywy">
+<cloud COLOR="#99ff66"/>
+<node CREATED="1507728715056" ID="ID_937473495" MODIFIED="1507728719180" TEXT="ngModel"/>
+</node>
+<node CREATED="1507728743008" ID="ID_1089975498" MODIFIED="1507728770830" POSITION="left" TEXT="&#x15a;rodowisko">
+<cloud COLOR="#ffffcc"/>
+</node>
+</node>
+</map>
