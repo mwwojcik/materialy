539bd3e061739abd55263c4fc6e58b3657f0b56e
mw <mw.wojcik@gmail.com>
2018-09-16 21:54:34 +0200

    aktualizacja

--- python-machine-learning/python-machine-learning.mm
+++ python-machine-learning/python-machine-learning.mm
@@ -9,7 +9,8 @@
 <node CREATED="1536927519010" ID="ID_564266397" LINK="https://www.wykop.pl/link/3121515/google-wystartowalo-serie-video-tutoriali-na-temat-uczenia-maszynowego/" MODIFIED="1536927530785" TEXT="https://www.wykop.pl/link/3121515/google-wystartowalo-serie-video-tutoriali-na-temat-uczenia-maszynowego/"/>
 <node CREATED="1536927552145" ID="ID_984845542" LINK="https://helion.pl/ksiazki/platforma-windows-azure-tejaswi-redkar-tony-guidici,plawia.htm#format/d" MODIFIED="1536927566566" TEXT="https://helion.pl/ksiazki/platforma-windows-azure-tejaswi-redkar-tony-guidici,plawia.htm#format/d"/>
 </node>
-<node CREATED="1537045849541" ID="ID_1784061390" MODIFIED="1537046006161" POSITION="left" TEXT="Python">
+<node CREATED="1537045849541" ID="ID_1784061390" MODIFIED="1537126530359" POSITION="left" TEXT="Python">
+<cloud COLOR="#ffcccc"/>
 <node CREATED="1537045856812" ID="ID_248408585" LINK="https://stackoverflow.com/questions/509211/understanding-pythons-slice-notation" MODIFIED="1537046189129" TEXT="Mechanizm wycinania tablic">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -75,11 +76,26 @@ a[-3::-1]&#160;&#160;# everything except the last two items, reversed</code></pr
 <node CREATED="1537080190167" ID="ID_703879907" LINK="https://www.pythonlikeyoumeanit.com/module_3.html" MODIFIED="1537080220045" TEXT="Modu&#x142; do oblicze&#x144; numerycznych NumPy"/>
 <node CREATED="1537080572644" ID="ID_879512837" LINK="https://www.pythonlikeyoumeanit.com/intro.html" MODIFIED="1537080622983" TEXT="pythonlikeyoumeanit - kompendium wiedzy o Pythonie i NumPy - Tutorial"/>
 </node>
-<node CREATED="1537104125211" ID="ID_254232834" MODIFIED="1537104136838" POSITION="right" TEXT="Tutoriale">
+<node CREATED="1537104125211" ID="ID_254232834" MODIFIED="1537126448788" POSITION="right" TEXT="Tutoriale">
+<cloud COLOR="#ffcc00"/>
 <node CREATED="1537104141395" ID="ID_1449012645" MODIFIED="1537104165194" TEXT="machinelearningmastery.com">
 <node CREATED="1537104170802" ID="ID_1870170993" LINK="https://machinelearningmastery.com/machine-learning-in-python-step-by-step/" MODIFIED="1537104222776" TEXT="Obr&#xf3;bka danych WE, tworzenie i por&#xf3;wnanie modelu,trening"/>
+<node CREATED="1537106804556" ID="ID_1361759728" LINK="https://machinelearningmastery.com/train-final-machine-learning-model/" MODIFIED="1537106830611" TEXT="Trening modelu"/>
 <node CREATED="1537104226493" ID="ID_218341857" LINK="https://machinelearningmastery.com/make-predictions-scikit-learn/" MODIFIED="1537106366025" TEXT="Tworzenie predykcji"/>
+<node CREATED="1537106908555" ID="ID_857249749" LINK="https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/" MODIFIED="1537106926993" TEXT="Serializacja modelu"/>
 </node>
+<node CREATED="1537108571258" ID="ID_1804021619" MODIFIED="1537108574478" TEXT="http://scikit-learn.org">
+<node CREATED="1537108577629" ID="ID_1859262591" LINK="http://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html#sphx-glr-auto-examples-classification-plot-digits-classification-py" MODIFIED="1537108609679" TEXT="Rozpoznawanie rozpikslowanych liczb"/>
+<node CREATED="1537109516698" ID="ID_1591753340" LINK="http://scikit-learn.org/stable/tutorial/statistical_inference/putting_together.html" MODIFIED="1537109529903" TEXT="Rozpoznawanie twarzy"/>
+</node>
+<node CREATED="1537125549573" FOLDED="true" ID="ID_597002908" LINK="www.kaggle.com" MODIFIED="1537126364576" TEXT="www.kaggle.com">
+<node CREATED="1537125562369" ID="ID_393793003" LINK="https://www.kaggle.com/jchen2186/machine-learning-with-iris-dataset" MODIFIED="1537125594294" TEXT="Rozpoznawanie gatunk&#xf3;w IRIS"/>
+<node CREATED="1537125621442" ID="ID_1591120814" LINK="https://www.kaggle.com/enerrio/scikit-learn-ml-from-start-to-finish" MODIFIED="1537125635912" TEXT="Titanic"/>
+</node>
+</node>
+<node CREATED="1537126457177" ID="ID_138049994" MODIFIED="1537126501888" POSITION="left" TEXT="Zbiory danych">
+<cloud COLOR="#ccffff"/>
+<node CREATED="1537126468540" ID="ID_1186323848" LINK="https://www.kaggle.com/datasets" MODIFIED="1537126484510" TEXT="https://www.kaggle.com/datasets"/>
 </node>
 </node>
 </map>
