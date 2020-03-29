0ac74b48e4374d1895de3d700e6a02fc62855ef6
mwwojcik <mw.wojcik@gmail.com>
2020-03-29 21:48:46 +0200

    act

---
+++ dna.mm
@@ -0,0 +1,198 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ffffcc" CREATED="1585423420139" ID="ID_1493379729" MODIFIED="1585509483430" TEXT="DNA">
+<font BOLD="true" NAME="SansSerif" SIZE="23"/>
+<node CREATED="1585423520402" HGAP="108" ID="ID_1017278685" MODIFIED="1585510861475" POSITION="right" TEXT="Modelowanie architektury C4" VSHIFT="6">
+<cloud COLOR="#9999ff"/>
+<node CREATED="1585423563343" ID="ID_804172323" MODIFIED="1585423570021" TEXT="Poziomy szczeg&#xf3;&#x142;owo&#x15b;ci">
+<node CREATED="1585423571443" ID="ID_1857196169" MODIFIED="1585439804832" TEXT="System">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Zestaw kilku kontener&#243;w realizuj&#261;cych wsp&#243;lne zadanie biznesowe.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1585439843659" ID="ID_1516907933" MODIFIED="1585509198015" TEXT="C1">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Diagram na tym poziomie pokazuje nam kto u&#380;ywa danego systemu. Z jakiego powodu on powsta&#322;, z kim wymienia swoje dane.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Punkt startu wiedzy o systemie.
+    </p>
+    <p>
+      
+    </p>
+    <div http-equiv="content-type" content="text/html; charset=utf-8" class="c-virtual_list__item" role="listitem" id="CNFDWA6G6-1585470122.083500-thread-list_1585470122.083500" data-qa="virtual-list-item" tabindex="-1">
+      <div role="document" class="c-message_kit__background c-message_kit__message c-message_kit__thread_message c-message_kit__thread_message--root" data-qa="message_container" data-qa-unprocessed="false" data-qa-placeholder="false">
+        <div role="document" class="c-message_kit__hover" data-qa-hover="true">
+          <div class="c-message_kit__actions c-message_kit__actions--inside">
+            <div class="c-message_kit__gutter">
+              <div class="c-message_kit__gutter__right" data-qa="message_content">
+                <div class="c-message_kit__blocks c-message_kit__blocks--rich_text">
+                  <div class="c-message__message_blocks c-message__message_blocks--rich_text">
+                    <div class="p-block_kit_renderer" data-qa="block-kit-renderer">
+                      <div class="p-block_kit_renderer__block_wrapper p-block_kit_renderer__block_wrapper--first">
+                        <div class="p-rich_text_block" dir="auto">
+                          <div class="p-rich_text_section">
+                            W jaki spos&#243;b definiujemy granice danego systemu na poziomie C1. Co sprawia, &#380;e dwa kontenery &#8220;nale&#380;&#261;&#8221; do jednego systemu? (Granice organizacyjne?)
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="c-message_kit__blocks c-message_kit__blocks--rich_text">
+                  <div class="c-message__message_blocks c-message__message_blocks--rich_text">
+                    <div class="p-block_kit_renderer" data-qa="block-kit-renderer">
+                      <div class="p-block_kit_renderer__block_wrapper p-block_kit_renderer__block_wrapper--first">
+                        <div class="p-rich_text_block" dir="auto">
+                          <div class="p-rich_text_section">
+                            Granice organizacyjne to dobry pomys&#322; i dobry wyznacznik granic, szczeg&#243;&#322;nie w niedu&#380;ych przedsi&#281;biorstwach. Ale s&#261; produkty, kt&#243;re integruj&#261; si&#281; ze starymi produktami w naszej organizacji (intereseriusze rozumiej&#261;, &#380;e to inne systemy) i w takim wypadku granic&#261; jest nowy system, tworzony przez nas i kt&#243;ry umo&#380;liwia nam now&#261; warto&#347;&#263; biznesow&#261;
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="c-virtual_list__item" role="listitem" id="CNFDWA6G6-1585470122.083500-thread-list_1585471970.084400" data-qa="virtual-list-item" tabindex="-1">
+      <div role="document" class="c-message_kit__background c-message_kit__background--hovered c-message_kit__message c-message_kit__thread_message" data-qa="message_container" data-qa-unprocessed="false" data-qa-placeholder="false">
+        <div class="c-message__message_blocks c-message__message_blocks--rich_text">
+          <div class="p-block_kit_renderer" data-qa="block-kit-renderer">
+            <div class="p-block_kit_renderer__block_wrapper p-block_kit_renderer__block_wrapper--first">
+              <div class="p-rich_text_block" dir="auto">
+                <div class="p-rich_text_section">
+                  wi&#281;c mo&#380;emy mie&#263; organizacj&#281;, kt&#243;ra w&#322;ada 4 systemami, tworzymy nowy, to b&#281;dzie 5 (jako nasz), a 4 jako &quot;zewn&#281;trzne&quot;, bo np musimy si&#281; z nimi integrowa&#263;, zarz&#261;dzaj&#261; nimi inne teamy, etc
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1585423577336" ID="ID_1300381984" MODIFIED="1585439769965" TEXT="Kontener">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#346;rodowisko uruchomieniowe dla komponent&#243;w. Jednostki gotowe na fizyczne wdro&#380;enie.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1585439936396" ID="ID_1447573369" MODIFIED="1585508974667" TEXT="C2">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Lista kontener&#243;w.
+    </p>
+    C2 - kontenery to nie musz&#261; by&#263; osobne jednotki wdro&#380;eniowe, tylko jednostki na to PRZYGOTOWANE
+
+    <p>
+      mog&#261; stanowi&#263; je grupy komponent&#243;w, kt&#243;re s&#261; faktycznie od siebie niezale&#380;ne (nieistotne jak je wdra&#380;amy) .
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1585423582374" ID="ID_1314991883" MODIFIED="1585439821845" TEXT="Komponent">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Zestaw kilku klas realizuj&#261;cych sp&#243;lne zadanie.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1585508983938" ID="ID_1644108288" MODIFIED="1585509035873" TEXT="C3">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    Komponent to zestaw klas, kt&#243;re razem tworz&#261; wy&#380;szego poziomu kontrakt. Mo&#380;e to by&#263; fasada oraz DTO.
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1585509328709" HGAP="62" ID="ID_1751279895" MODIFIED="1585510859924" POSITION="left" TEXT="DDD">
+<cloud COLOR="#ccffcc"/>
+<node CREATED="1585509356321" ID="ID_1651176478" MODIFIED="1585509504174" TEXT="DDD by example - library">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    Cze&#347;&#263;. Od jakiego&#347; czasu stawiam swoje pierwsze kroki w &#347;wiecie DDD intensywnie wykorzystuj&#261;c do tego projekt
+
+    <p>
+      <a http-equiv="content-type" content="text/html; charset=utf-8" target="_blank" class="c-link" delay="150" aria-describedby="slack-kit-tooltip" href="https://slack-redir.net/link?url=https%3A%2F%2Fgithub.com%2Fddd-by-examples%2Flibrary&amp;v=3" rel="noopener noreferrer">https://github.com/ddd-by-examples/library</a>&#160;.<br />&#379;eby przyswoi&#263; sobie techniki tam zaprezentowane, dokonuj&#281; jego ponownej reimplementacji (na t&#281; chwil&#281; bardzo odtw&#243;rczej, ale jednak reimplementacji <img src="https://a.slack-edge.com/production-standard-emoji-assets/10.2/google-medium/1f642.png" aria-label="slightly smiling face emoji" alt=":slightly_smiling_face:" data-stringify-type="emoji" data-stringify-emoji=":slightly_smiling_face:" />).<br />Mam pewien problem ze zrozumieniem granic wyodr&#281;bnionych tam kontekst&#243;w.<br />Mianowicie, odkryte niezmienniki dotycz&#261; dw&#243;ch byt&#243;w: Rezerwacji i Wypo&#380;yczenia<br />(<a target="_blank" class="c-link" delay="150" aria-describedby="slack-kit-tooltip" href="https://slack-redir.net/link?url=https%3A%2F%2Fgithub.com%2Fddd-by-examples%2Flibrary%2Fblob%2Fmaster%2Fdocs%2Fimages%2Fexample-mapping.png&amp;v=3" rel="noopener noreferrer">https://github.com/ddd-by-examples/library/blob/master/docs/images/example-mapping.png</a>) . Co takiego nale&#380;a&#322;o zauwa&#380;y&#263;, &#380;e zosta&#322;y one po&#322;&#261;czone w jeden wsp&#243;lny kontekst (lending). Jaki czynnik zdecydowa&#322; &#380;e nie wyodr&#281;bnione zosta&#322;y dwa oddzielne konteksty, po jednym dla Hold i Checkout ?&#160;(edited)&#160;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    hold i checkout to przypadki u&#380;ycia z perspektywy klienta. Jest niezmiennik kt&#243;ry przecina oba te przypadki (2 sp&#243;&#378;nione checkouty i nie mo&#380;esz robi&#263; hold). W dna nazywamy to heurystyka antywymagan - by&#322;yby wtedy 2 konteksty przeci&#281;te niezmiennikiem
+
+    <p>
+      &#160;(edited
+    </p>
+    <p>
+      
+    </p>
+    By&#322; niezmiennik na wp&#322;yw kt&#243;rego by&#322;y 2 komendy - wiec wpad&#322;o w jeden agregat. Wiec w 1 kontekst :) &#380;eby zawsze by&#322;a sp&#243;jno&#347;&#263;
+
+    <p>
+      
+    </p>
+    Jakby by&#322;y obs&#322;ugiwane osobno, to by&#322;aby szansa, ze hold si&#281; nie uda, mimo ze masz 4 rezerwacje, 5 rezerwacje w&#322;a&#347;nie kto&#347; za Ciebie odebra&#322; (checkout) ale ciagle masz w systemie 5 (wy&#347;cig). W bibliotece pewnie to nie jest problem, chodzi&#322;o o dydaktyczne przekazanie idei agregatu
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1585510867352" ID="ID_9447650" MODIFIED="1585511238953" POSITION="right" TEXT="Architektura">
+<cloud COLOR="#ffffcc"/>
+</node>
+<node CREATED="1585511135630" HGAP="46" ID="ID_338746357" MODIFIED="1585511244881" POSITION="left" TEXT="Linki" VSHIFT="21">
+<cloud COLOR="#ff9900"/>
+<node CREATED="1585511140114" ID="ID_1750157851" LINK="https://dna-pl.slack.com/" MODIFIED="1585511147917" TEXT="https://dna-pl.slack.com/"/>
+<node CREATED="1585511153616" ID="ID_1092957864" LINK="https://edu.devstyle.pl/architektura/" MODIFIED="1585511182554" TEXT="https://edu.devstyle.pl/architektura/"/>
+<node CREATED="1585511205663" ID="ID_11883161" LINK="https://structurizr.com/dashboard" MODIFIED="1585511212762" TEXT="https://structurizr.com/dashboard"/>
+</node>
+</node>
+</map>
